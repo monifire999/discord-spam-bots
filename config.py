c3ca9b604c7fa6f9ad6a6242ca6a2ea720b760c5
monifire999 <98234736+monifire999@users.noreply.github.com>
2022-03-26 22:03:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Basic Config (For all bots)
 pythonCommand = "python" # If you have the python command set to something else such as python3
-userToken = [''] # User Tokens here. example 'token','token' | 'email:pass', 'token'
-DiscordChannel = '' # Channel ID,
+userToken = ['ODMxMDY1NjE0MjMxNjY2NzY4.Yj8qCw.1CHMUtppjLzMHqT90OQFQ_sP9CE'] # User Tokens here. example 'token','token' | 'email:pass', 'token'
+DiscordChannel = '957292846350729338' # Channel ID,
 SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 textRandom = False # if the root has 'text.txt' it will pick a random text line and send it.
 textFull = False # Will send everything written in 'text.txt'
@@ -9,13 +9,13 @@ discumLog = False # Displays Discums Logs, Very useful for providing more info o
 
 #Server Joiner
 useTokenJoin = True
-inviteLink = '' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using email join then just the full URL
+inviteLink = 'https://discord.gg/Td8x5n4q' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using email join then just the full URL
 autojoinServer = False # Will auto join the inviteLink server if True
 useBrowser = 'Chrome' # Chrome | Firefox | Edge (windows only) | Set the browser used for joining the servers.
 joinSpeed = 3 # Changes how fast the bot will start the server joiner
 
 #DM Spam
-DiscordServer = '' # Server ID (https://i.imgur.com/m06TSEx.png this one)
+DiscordServer = 'https://discord.gg/Td8x5n4q' # Server ID (https://i.imgur.com/m06TSEx.png this one)
 ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
 HeavyScrape = False # (True or False) Use this for servers with a lot of users
 
